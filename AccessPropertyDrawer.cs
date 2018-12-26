66172a8fbb82eca08b2e9b0a1f65388c53b0e2b4
DoDoBooM <43022779+DoDoBooM@users.noreply.github.com>
2018-12-26 15:25:02 +0800

    Add files via upload

---
+++ AccessPropertyDrawer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+[CustomPropertyDrawer(typeof(RangeProperty))]
+public class AccessPropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginChangeCheck();
+        RangeProperty rp = attribute as RangeProperty;
+        EditorGUI.Slider(position, property, rp.min, rp.max);
+        if (EditorGUI.EndChangeCheck())
+        {
+            rp.dirty = true;
+        }
+        else if (rp.dirty)
+        {
+            object obj = property.serializedObject.targetObject;
+            Type type = obj.GetType();
+            PropertyInfo pi = type.GetProperty(rp.propertyName);
+            if (pi != null)
+            {
+                pi.SetValue(obj, fieldInfo.GetValue(obj), null);
+            }
+            else
+            {
+                Debug.LogError("Invalid property name: " + rp.propertyName + "\nCheck your [RangeProperty] attribute!");
+            }
+            rp.dirty = false;
+        }
+    }
+}
---
+++ RangeProperty.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class RangeProperty : PropertyAttribute
+{
+    public string propertyName { get; private set; }
+    public float min { get; private set; }
+    public float max { get; private set; }
+    public bool dirty { get; set; }
+    public RangeProperty(string propertyName, float min, float max)
+    {
+        this.propertyName = propertyName;
+        this.min = min;
+        this.max = max;
+    }
+}
